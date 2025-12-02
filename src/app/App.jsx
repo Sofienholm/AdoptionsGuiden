9db9934fcc96e5a0851c8801a342b63fb344b693
Sofienholm <eaa24snho@students.eaaa.dk>
2025-12-02 15:43:03 +0100

    step 2

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,13 +1,15 @@
 import { Routes, Route } from "react-router";
 import Onboard from "../pages/onboard/Onboard";
 import Landing from "../pages/landing/landing";
+import Step2 from "../pages/onboard/storytelling/Step2/step2.jsx";
 
 function App() {
   return (
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/onboard" element={<Onboard />} />
-      </Routes>
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/onboard" element={<Onboard />} />
+      <Route path="/onboard/step2" element={<Step2 />} />
+    </Routes>
   );
 }
 export default App;
