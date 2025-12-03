ea1debd09076aaa9b710d3fee494e9c027430f9f
Martinlpilgaard <eaa24mlpk@students.eaaa.dk>
2025-12-03 12:22:20 +0100

    step 4 opstart

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router";
 import Onboard from "../pages/onboard/Onboard";
 import Landing from "../pages/landing/landing";
 import Step2 from "../pages/onboard/storytelling/Step2/step2.jsx";
+import Step4 from "../pages/onboard/storytelling/Step4/step4.jsx";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Landing />} />
       <Route path="/onboard" element={<Onboard />} />
       <Route path="/onboard/step2" element={<Step2 />} />
+      <Route path="/onboard/step4" element={<Step4 />} />
     </Routes>
   );
 }
