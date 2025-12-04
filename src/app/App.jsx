7ef1a77f348828979cc0418cb01ba2640d4ccaf0
Martinlpilgaard <eaa24mlpk@students.eaaa.dk>
2025-12-04 15:34:55 +0100

    done

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,6 +3,7 @@ import Onboard from "../pages/onboard/Onboard";
 import Landing from "../pages/landing/landing";
 import Step2 from "../pages/onboard/storytelling/Step2/step2.jsx";
 import Step4 from "../pages/onboard/storytelling/Step4/step4.jsx";
+import Step5 from "../pages/onboard/storytelling/Step5/step5.jsx";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/onboard" element={<Onboard />} />
       <Route path="/onboard/step2" element={<Step2 />} />
       <Route path="/onboard/step4" element={<Step4 />} />
+      <Route path="/onboard/step5" element={<Step5 />} />
     </Routes>
   );
 }
