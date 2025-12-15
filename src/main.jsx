d20e17fab0c5cc6b7bf6588d39c3dd4ebfd7d4d0
Sofienholm <eaa24snho@students.eaaa.dk>
2025-12-15 12:58:57 +0100

    fix2#

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import { StrictMode } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app/App.jsx";
 import { BrowserRouter } from "react-router";
 import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>
+  </React.StrictMode>
 );
