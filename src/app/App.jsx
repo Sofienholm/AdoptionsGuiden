199dfee6e12d914800165b846fc2faa3de1ef299
Sofienholm <eaa24snho@students.eaaa.dk>
2025-12-15 12:47:25 +0100

    m

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -22,7 +22,7 @@ import Onboard from "../pages/onboard/Onboard";
 import Step1 from "../pages/onboard/storytelling/Step1/Step1.jsx";
 import Step2 from "../pages/onboard/storytelling/Step2/step2.jsx";
 import Step3 from "../pages/onboard/storytelling/Step3/step3.jsx";
-import Step4 from "../pages/onboard/storytelling/Step4/Step4.jsx";
+import Step4 from "../pages/onboard/storytelling/Step4/step4.jsx";
 import Step5 from "../pages/onboard/storytelling/Step5/step5.jsx";
 
 //Quiz intro & flow
