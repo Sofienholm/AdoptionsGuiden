86c793f73b5f92d5c0b47defc5772caf2db48157
Sofienholm <eaa24snho@students.eaaa.dk>
2025-12-16 12:18:13 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+  <BrowserRouter basename={import.meta.env.DEV ? "/" : "/AdoptionsGuiden/"}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
