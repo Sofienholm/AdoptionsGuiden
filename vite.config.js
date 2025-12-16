96d674e124fbe3c856b9c516760a415fbcecfee3
Sofienholm <eaa24snho@students.eaaa.dk>
2025-12-16 12:10:04 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
+export default defineConfig(({ command }) => {
+  return {
   plugins: [
     react(),
     tailwindcss(), 
   ],
+    base: command === "serve" ? "/" : "/AdoptionsGuiden/"
+  };
 });
